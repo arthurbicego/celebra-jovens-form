65c42ef4ecf0518a58bd7ebee2ed6ff50a664590
arthurbicego <arthurbq5@gmail.com>
2024-07-15 09:38:05 -0300

    commit user test

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ require_once($__ROOT__ . "/logic.php");
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- commit test -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
